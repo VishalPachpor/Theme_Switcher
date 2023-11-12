206ccc34fcc3d56235775edff8a88e8d60108dda
VishalPachpor <Vishalpatil080502@gmail.com>
2023-11-12 12:30:52 +0530

    Components added with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
+import { useState } from 'react'
+import Card from './components/Card'
+import ThemeBtn from './components/ThemeBtn'
+import { ThemeProvider } from './context/themeContext'
 
-import './App.css'
 
 function App() {
+    const [themeMode, setThemeMode] = useState("light");
 
+    const darkTheme = ()=>{
+      setThemeMode("dark")
+    }
+    const lightTheme = ()=>{
+      setThemeMode("light")
+    }
 
   return (
-    <>
+    <ThemeProvider value={{darkTheme, lightTheme, themeMode}}>
 
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-
+              <ThemeBtn />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-
+              <Card />
           </div>
         </div>
       </div>
 
 
-    </>
+    </ThemeProvider>
   )
 }
 
